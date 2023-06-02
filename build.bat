0635f4ba1a209dbefa31311756059f7089fa5852
RadostW <radost.waszkiewicz@gmail.com>
2023-06-02 21:47:55 +0200

    Windows build

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
-export CIBW_BUILD="cp3[891]*-win_amd64"
-export CIBW_SKIP="cp31[12]*"
-export CIBW_BEFORE_BUILD="python3 -m pip install -U pip wheel numpy==1.22"
-cibuildwheel --platform windows
+set CIBW_BUILD=cp3[891]*-win_amd64
+set CIBW_SKIP=cp31[12]*
+set CIBW_BEFORE_BUILD=python.exe -m pip install -U pip wheel numpy==1.22
+
+python.exe -m cibuildwheel --platform windows
